ad5047f328e9f67f3bb68ce235c865442a5e48db
Eudes G.G Duarte <eudesgracias.duarte@gmail.com>
2025-10-17 16:51:44 +0100

    chore(themes): Costume radix-themes pallete

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Theme } from "@radix-ui/themes"
-
 import "@radix-ui/themes/styles.css"
 
 import './index.css'
-import App from './App.tsx'
+import App from './app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
