03c5b994552c8af2d69f64472bff4da4d0f1490a
Eudes G.G Duarte <eudesgracias.duarte@gmail.com>
2025-10-18 00:19:13 +0100

    chore(config): vite config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 
+const apiUrl = process.env.VITE_API_URL || "http://localhost:8090"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,7 +13,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:8090', // need to be env variable
+      '/api': apiUrl
     },
   },
 })
