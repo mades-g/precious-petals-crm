d26215779d1d24cf15a2eafb7c9b71834146f97f
Eudes G.G Duarte <eudesgracias.duarte@gmail.com>
2025-11-26 15:25:31 +0000

    chore(fix): APP base url

--- apps/frontend/src/services/pb/client.ts
+++ apps/frontend/src/services/pb/client.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "@/services/pb/types";
 
-const baseUrl = import.meta.env.VITE_PB_URL || "http://127.0.0.1:8090";
+const baseUrl = import.meta.env.VITE_APP_URL || "http://127.0.0.1:8090";
 
 export const pb = new PocketBase(baseUrl) as TypedPocketBase;
 
